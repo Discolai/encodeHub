dee99b24dc4fb2763f2636299abe80dd55d53f6a
Nikolai Norum Hansen <nikolainh@hotmail.com>
2019-12-10 21:08:16 +0100

    Added state saving with pickle

--- encodeHub_node.py
+++ encodeHub_node.py
@@ -2,7 +2,30 @@ import json, time
 import threading
 import api
 import globals
+import pickle
 from ffmpeg import FFmpeg
+
+def handle_job(m, config):
+    print("Got job: ", m)
+
+    input = "\"%s\""%(m)
+    output = "\"%shevc.mkv\""%(m[:-3])
+
+    job = FFmpeg(config["ffmpeg"]["inargs"], input, config["ffmpeg"]["outargs"], output)
+
+    job.start()
+
+    while not job.has_finished():
+        if globals.paused:
+            job.pause()
+            while globals.paused:
+                time.sleep(config["sleeptime"])
+            job.resume()
+
+        progress = job.read_progress()
+        globals.progress_q.append(progress)
+        if len(globals.progress_q) > 1:
+            globals.progress_q.popleft()
 
 def main():
     api_thread = threading.Thread(target=api.run, daemon=True)
@@ -14,30 +37,23 @@ def main():
         while len(globals.job_q) == 0:
             time.sleep(config["sleeptime"])
 
-        m = globals.job_q.popleft()
-        print("Got job: ", m)
-
-        input = "\"%s\""%(m)
-        output = "\"%shevc.mkv\""%(m[:-3])
-
-        job = FFmpeg(config["ffmpeg"]["inargs"], input, config["ffmpeg"]["outargs"], output)
-
-        job.start()
-
-        while not job.has_finished():
-            if globals.paused:
-                job.pause()
-                while globals.paused:
-                    time.sleep(config["sleeptime"])
-                job.resume()
-
-            progress = job.read_progress()
-            globals.progress_q.append(progress)
-            if len(globals.progress_q) > 1:
-                globals.progress_q.popleft()
-
+        try:
+            m = globals.job_q.popleft()
+            handle_job(m, config)
+        except:
+            globals.job_q.appendleft(m)
+            raise e
 
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except:
+        with open(globals.progress_q_pickle, "wb") as f:
+            pickle.dump(globals.progress_q, f)
+        with open(globals.job_q_pickle, "wb") as f:
+            pickle.dump(globals.job_q, f)
+        with open(globals.paused_pickle, "wb") as f:
+            picle.dump(globals.paused)
+        exit()
--- globals.py
+++ globals.py
@@ -1,5 +1,24 @@
 from collections import deque
+import pickle
 
-paused = False
-progress_q = deque()
-job_q = deque()
+paused_pickle = "paused_pickle.pkl"
+try:
+    with open(paused_pickle, "rb") as f:
+        paused_pickle = pickle.load(f)
+except:
+    paused = False
+
+progress_q_pickle = "progress_q_pickle.pkl"
+try:
+    with open(progress_q_pickle, "rb") as f:
+        progress_q = pickle.load(f)
+except:
+    progress_q = deque()
+
+
+job_q_pickle = "job_q_pickle.pkl"
+try:
+    with open(job_q_pickle, "rb") as f:
+        job_q = pickle.load(f)
+except:
+    job_q = deque()
