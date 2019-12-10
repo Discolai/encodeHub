b796dc2d8a7aef0dfa55d5532fc5f226f0a8bb56
Nikolai Norum Hansen <nikolainh@hotmail.com>
2019-12-10 17:20:59 +0100

    Rewrote it using pexpect

--- encode.py
+++ encode.py
@@ -1,84 +1,91 @@
-import json, datetime, subprocess, time, io, re
+import json, datetime, subprocess, time, io, re, signal, pexpect
+from pydoc import locate
+
+class DurationError(Exception):
+    pass
 
 class FFmpeg:
     """docstring for FFmpeg."""
 
-    progress_reg = re.compile(
-        "".join([
-            "^frame=(?P<frame>.+)",
-            "fps=(?P<fps>.+)",
-            "stream.+=(?P<stream>.+)",
-            "bitrate=(?P<bitrate>.+)kbits/s",
-            "total_size=(?P<total_size>.+)",
-            "out_time_ms=(?P<out_time_ms>.+)",
-            "out_time=(?P<out_time>.+)",
-            "dup_frames=(?P<dup_frames>.+)",
-            "drop_frames=(?P<drop_frames>.+)",
-            "speed=(?P<speed>.+)x",
-            "progress=(?P<progress>.+)"
-        ])
-    )
+    formats = {
+        "frame": (re.compile(b"frame=(?P<frame>.+)"), "int"),
+        "fps": (re.compile(b"fps=(?P<fps>.+)"), "float"),
+        "stream": (re.compile(b"stream.+=(?P<stream>.+)"), "str"),
+        "bitrate": (re.compile(b"bitrate=(?P<bitrate>.+)kbits/s"), "float"),
+        "total_size": (re.compile(b"total_size=(?P<total_size>.+)"), "int"),
+        "out_time_ms": (re.compile(b"out_time_ms=(?P<out_time_ms>.+)"), "int"),
+        "out_time": (re.compile(b"out_time=(?P<out_time>.+)"), "str"),
+        "dup_frames": (re.compile(b"dup_frames=(?P<dup_frames>.+)"), "int"),
+        "drop_frames": (re.compile(b"drop_frames=(?P<drop_frames>.+)"), "int"),
+        "speed": (re.compile(b"speed=(?P<speed>.+)x"), "float"),
+        "progress":(re.compile(b"progress=(?P<progress>.+)"), "str")
+    }
 
 
     def __init__(self, inargs, input, outargs, output):
         self.cmd = " ".join(["ffmpeg", *inargs, "-i", input, *outargs, output])
 
-        ffprobe_cmd = "ffprobe -v error -sexagesimal -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 " + input
-        self.duration = subprocess.run(ffprobe_cmd, stdout=subprocess.PIPE, shell=True).stdout.strip().decode("utf-8")
-        self.duration = hhmmssms_to_ms(self.duration)
-
         self._p = None
         self.progress = None
+        self._finished = False
 
     def start(self):
-        self._p = subprocess.Popen(self.cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-
-    def discard_info(self):
-        while self._p.stdout.readline()[:8] != b"progress":
-            pass
+        self._p = pexpect.spawn(self.cmd)
+        self._get_duration()
+        self.exp_list = self._p.compile_pattern_list([pexpect.EOF, "(.+)"])
 
     def has_finished(self):
-        return self._p.returncode != None
+        return self._finished
 
-    def _current_progress(self):
-        return int((self.progress["out_time_s"]/self.duration) * 100)
+    def _get_duration(self):
+        try:
+            self._p.expect("Duration: (?P<duration>.+), start:", timeout=5)
+            self.duration = hhmmssms_to_ms(self._p.match.group("duration").decode("utf-8").strip())
+        except pexpect.exceptions.TIMEOUT as e:
+            raise DurationError("Unable to get video duration!")
 
-    def _remaining_time(self):
-        return ms_to_hhmmssms(self.duration-(self.progress["out_time_ms"])/self.progress["speed"])
+    def _current_progress(self, out_time, duration):
+        return int((out_time/duration) * 100)
 
-    def _cast_progress(self, progress):
-        # Since ffmpeg for some reason states ms as microseconds instead of milliseconds
-        # This workaround takes into account that it might get fixed in the future
-        ms = hhmmssms_to_ms(progress["out_time"])
-        return {
-            "frame": int(progress["frame"]),
-            "fps": float(progress["fps"]),
-            "stream": progress["stream"],
-            "bitrate": float(progress["bitrate"]),
-            "total_size": int(progress["total_size"]),
-            "out_time_ms": ms,
-            "out_time_s": int(ms/1000),
-            "out_time": progress["out_time"],
-            "dup_frames": int(progress["dup_frames"]),
-            "drop_frames": int(progress["drop_frames"]),
-            "speed": float(progress["speed"]),
-            "progress": progress["progress"],
-        }
+    def _remaining_time(self, out_time, duration, speed):
+        return ms_to_hhmmssms((duration-out_time)/speed)
+
+    def auto_cast(self, progress):
+        res = {}
+        for key, v in self.formats.items():
+            match = re.search(v[0], progress)
+            if match:
+                if v[1] == "str":
+                    res[key] = match.group(key).strip().decode("utf-8")
+                else:
+                    res[key] = locate(v[1])(match.group(key).strip())
+            else:
+                res[key] = None
+
+        return res
+
 
     def read_progress(self):
-        out = b""
-        for i in range(11):
-            out += self._p.stdout.readline().strip()
-
-        match = re.search(self.progress_reg, out.decode("utf-8"))
-        if match:
-            self.progress = self._cast_progress(match.groupdict())
-
-            self.progress["progress"] = self._current_progress()
-            self.progress["remaining"] = self._remaining_time()
+        i = self._p.expect_list(self.exp_list)
+        if i == 0:
+            self._finished = True
+        else:
+            out = self._p.match.group(i)
+            self.progress = self.auto_cast(out)
+            try:
+                if self.progress["progress"] in ["end", "stop"]:
+                    self._p.kill(signal.SIGINT)
+            except Exception as e:
+                pass
+            try:
+                ms = hhmmssms_to_ms(self.progress["out_time"])
+                self.progress["percentage"] = self._current_progress(ms, self.duration)
+                self.progress["remaining"] = self._remaining_time(ms, self.duration, self.progress["speed"])
+            except Exception as e:
+                print(self.progress)
+                self.progress["percentage"] = None
+                self.progress["remaining"] = None
         return self.progress
-
-
 
 
 def hhmmssms_to_ms(formatted):
@@ -112,16 +119,17 @@ def main():
         job = FFmpeg(config["ffmpeg"]["inargs"], input, config["ffmpeg"]["outargs"], output)
 
         job.start()
-        job.discard_info()
 
         while not job.has_finished():
             progress = job.read_progress()
-            print("Frame: {}\tFps: {}\tProgress: {}%\t Remaining: {}\tOut time: {}".format(
+            print("Frame: {}\tFps: {}\tProgress: {}%\t Remaining: {}\tSpeed: {}\tOut time: {}\tDuration: {}".format(
                 progress["frame"],
                 progress["fps"],
-                progress["progress"],
+                progress["percentage"],
                 progress["remaining"],
-                progress["out_time"]
+                progress["speed"],
+                progress["out_time"],
+                ms_to_hhmmssms(job.duration)
             ))
 
         print("stop: ", datetime.datetime.now(), "\n")
