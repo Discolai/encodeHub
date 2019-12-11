13e414ebe93c2725734951dcad48ec07eeefbaad
Nikolai Norum Hansen <nikolainh@hotmail.com>
2019-12-11 21:10:38 +0100

    Added node and logging endpoints. And Chanhed the sqlite row factory

--- distributor.py
+++ distributor.py
@@ -9,6 +9,7 @@ def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
+        db.row_factory = sqlite3.Row
     return db
 
 @app.teardown_appcontext
@@ -18,11 +19,11 @@ def close_connection(exception):
         db.commit()
         db.close()
 
-@app.route("/jobs/<id>", methods=["DELETE"])
-def delete(id):
+@app.route("/jobs/<jid>", methods=["DELETE"])
+def delete(jid):
     cur = get_db().cursor()
     try:
-        cur.execute("delete from jobs where id=(?);", (id,))
+        cur.execute("delete from jobs where jid=(?);", (jid,))
         return jsonify({"err": None}), 200
     except Exception as e:
         return jsonify({"err": str(e)}), 400
@@ -37,9 +38,9 @@ def insert():
                 try:
                     cur.execute("insert into jobs (job) values (?)", (job,))
                 except Exception as e:
-                    return jsonify({"err": str(e)}), 500
+                    return jsonify({"err": "Duplicate job entry {}".format(job)}), 400
             else:
-                return jsonify({"err": "\"{}\" is an invalid job!"}), 400
+                return jsonify({"err": "{} is an invalid job!".format(job)}), 400
         return jsonify({"err": None}), 200
     else:
         return jsonify({"err": "Wrong json formatting, expected array!"}), 400
@@ -49,7 +50,7 @@ def jobs():
     cur = get_db().cursor()
 
     cur.execute("select * from jobs;")
-    jobs = cur.fetchall()
+    jobs = [dict(job) for job in cur.fetchall()]
     if len(jobs):
         return jsonify({"err": None, "jobs": jobs}), 200
     else:
@@ -59,16 +60,77 @@ def jobs():
 def oldest():
     cur = get_db().cursor()
 
-    cur.execute("select id, job, MIN(timestamp) from jobs where processing = 0;")
-    job = cur.fetchall()
-    if job and job[0] and job[0][0]:
-        try:
-            cur.execute("update jobs set processing = 1 where id = ?;", (job[0][0],))
-            return jsonify({"err": None, "job": job[0]})
-        except Exception as e:
-            return jsonify({"err": str(e)}), 400
-    return jsonify({"err": None}), 204
+    node = request.get_json()
+    if "name" not in node:
+        return jsonify({"err": "No node specified!", "job": None}), 400
 
+    cur.execute("select nid from nodes where name = ?;", (node["name"],))
+    nid = dict(cur.fetchall()[0])["nid"]
+    if not nid:
+        return jsonify({"err": "Could not find node: {}".format(node["name"])}), 404
+
+    cur.execute("select jid, job, MIN(timestamp) as timestamp from jobs where nid is null;")
+    job = dict(cur.fetchall()[0])
+    print(job)
+
+    if not job or not job["jid"]:
+        return jsonify({"err": "Job queue is empty!"}), 404
+
+    cur.execute("update jobs set nid = 1 where jid = ?;", (job["jid"],))
+    return jsonify({"err": None, "job": job})
+
+@app.route("/nodes", methods=["GET"])
+def nodes():
+    cur = get_db().cursor()
+    cur.execute("select * from nodes;")
+    nodes = [dict(node) for node in cur.fetchall()]
+    if len(nodes):
+        return jsonify({"err": None, "nodes": nodes}), 200
+    else:
+        return jsonify({"err": "No nodes registered!", "nodes": None}), 404
+
+
+@app.route("/logs/<node>", methods=["GET"])
+def get_logs(node):
+    cur = get_db().cursor()
+    sql = (
+        "select * from logs as l"
+        "join (select * from nodes where name = ?)"
+        " as n on l.node = n.id;"
+    )
+    cur.execute("select * from logs natural join (select * from nodes where name = ?)", (node,))
+    logs = [dict(row) for row in cur.fetchall()]
+
+    if len(logs):
+        return jsonify({"err": None, "logs": logs}), 200
+    else:
+        return jsonify({"err": "{} has no logs".format(node), "logs": None}), 404
+
+
+@app.route("/logs/<node>", methods=["POST"])
+def post_log(node):
+    # Get the node's id
+    cur.execute("select nid from nodes where name = ?;", (node,))
+    nid = dict(cur.fetchall()[0])["nid"]
+    if not nid:
+        return jsonify({"err": "Could not find node: {}".format(node)}), 404
+    l = request.get_json()
+    args = (
+        nid, l["frame"], l["fps"], l["speed"], l["bitrate"], l["drop_frames"],
+        l["dup_frames"], l["stream"], l["elapsed_time"], l["out_time"],
+        l["remaining_time"], l["percentage"], l["progress"], l["video"],
+        l["audio"], l["subtitle"], l["global_headers"], l["other_streams"],
+        l["total_size"], l["muxing_overhead"]
+    )
+    sql = (
+        "insert into logs "
+        "(nid, frame, fps, speed, bitrate, drop_frames, dup_frames, stream,"
+        "elapsed_time, out_time, remaining_time, percentage, progress,"
+        "video, audio, subtitle, global_headers, other_streams, total_size, muxing_overhead) "
+        "values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"
+    )
+    cur.execute(sql, args)
+    return jsonify({"err": None}), 200
 
 def main():
     from waitress import serve
