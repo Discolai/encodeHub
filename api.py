01a7141fff6323dc87db082db5c159146a49346e
Nikolai Norum Hansen <nikolainh@hotmail.com>
2019-12-11 15:23:32 +0100

    Rewrote and added some api endpoints

--- api.py
+++ api.py
@@ -3,36 +3,51 @@ import json, shlex, os, globals, signal
 
 app = Flask(__name__)
 
-@app.route("/enque/<new_job>", methods=["GET", "POST"])
-def jobs(new_job):
-    if request.method == "GET":
-        if len(globals.job_q) == 0:
-            return Response(status=404)
-        job = globals.job_q.popleft()
-        globals.job_q.appendleft(job)
-        return jsonify(job)
+@app.route("/enque", methods=["POST"])
+def enque():
+    nj = request.get_json()
+    if nj and "job" in nj and isinstance(nj["job"], list) and len(nj["job"]) == 3:
+        nj["job"][1] = shlex.quote(nj["job"][1])
+        globals.job_q.append(nj["job"])
+        return jsonify({"err": None}), 200
     else:
-        globals.job_q.append(shlex.quote(new_job))
-        return Response(status=200)
+        return jsonify({"err": "Invalid json"}), 400
+
+@app.route("/deque", methods=["POST"])
+def deque():
+    try:
+        job = globals.job_q.popleft()
+        return jsonify({"err": None, "job": job}), 200
+    except:
+        return jsonify({"err": "Job queue is empty", "job": None}), 404
+
+@app.route("/queue", methods=["GET"])
+def queue():
+    jobs = list(globals.job_q)
+    if len(jobs):
+        return jsonify({"err": None, "jobs": jobs}), 200
+    else:
+        return jsonify({"err": "Job queue is empty", "jobs": None}), 404
+
+
+@app.route("/progress", methods=["GET"])
+def progress():
+    ret = {"paused": globals.paused}
+    if len(globals.progress_q) > 0:
+        progress = globals.progress_q[0]
+        return jsonify({"err": None, "progress": progress})
+    else:
+        return jsonify({"err": "No progress available", "progress": None}), 404
 
 @app.route("/pause", methods=["POST"])
 def pause():
     globals.paused = not globals.paused
-    return Response(status=200)
-
-@app.route("/status", methods=["GET"])
-def job():
-    ret = {"paused": globals.paused}
-    if len(globals.progress_q) > 0:
-        progress = globals.progress_q.popleft()
-        globals.progress_q.appendleft(progress)
-        ret = {**ret, **progress}
-    return jsonify(ret)
+    return jsonify({"err": None, "paused": globals.paused}), 200
 
 @app.route("/stop", methods=["POST"])
 def stop():
     globals.stop = True
-    return Response(status=200)
+    return Response(status=204)
 
 def run():
     from waitress import serve
