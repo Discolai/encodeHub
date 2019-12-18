c7b7a43e91c8e80b860e09143787ce06b47ddb50
Nikolai Norum Hansen <nikolainh@hotmail.com>
2019-12-18 02:58:56 +0100

    Removed some unsignificant table values

--- distributor/frontend/src/components/logs/logItem.js
+++ distributor/frontend/src/components/logs/logItem.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 class LogItem extends React.Component {
   render () {
-    const {lid, jid, nid, audio, video, subtitle, global_headers, other_streams, lsize, prev_size, muxing_overhead, drop_frames, dup_frames, elapsed_time} = this.props.log;
+    const {lid, jid, nid, audio, video, subtitle, lsize, prev_size, elapsed_time} = this.props.log;
     return (
       <tr>
         <th>{lid}</th>
@@ -12,13 +12,8 @@ class LogItem extends React.Component {
         <td>{audio} kiB</td>
         <td>{video} kiB</td>
         <td>{subtitle} kiB</td>
-        <td>{global_headers} kiB</td>
-        <td>{other_streams} kiB</td>
         <td>{lsize} kiB</td>
         <td>{prev_size} kiB</td>
-        <td>{muxing_overhead}</td>
-        <td>{drop_frames}</td>
-        <td>{dup_frames}</td>
         <td>{elapsed_time}</td>
       </tr>
     );
