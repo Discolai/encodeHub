b2f8faeeb7a6efb2fcfa5316585607a325b93919
Nikolai Norum Hansen <nikolainh@hotmail.com>
2019-12-19 18:23:35 +0100

    Abstracted a node into a separate component

--- distributor/frontend/src/components/nodes/cNode.js
+++ distributor/frontend/src/components/nodes/cNode.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import axios from 'axios'
 import NodeItem from './nodeItem'
 import LogItem from '../logs/logItem'
@@ -7,44 +6,20 @@ import Navbar from '../navbar'
 
 import Container from 'react-bootstrap/Container'
 import Table from 'react-bootstrap/Table'
-import ProgressBar from 'react-bootstrap/ProgressBar'
-
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faInfoCircle, faBuilding, faLocationArrow, faClock} from '@fortawesome/free-solid-svg-icons'
 
 class Node extends React.Component {
   state = {
-    node: null,
     logs: [],
-    progress: null
-  }
-  timer = null;
+  };
 
   componentDidMount() {
-    this.timer = setInterval(() => this.getProgress(), 2000)
-    this.getNode(this.props.match.params.nid);
     this.getLogs(this.props.match.params.nid);
-    this.getProgress()
   }
 
   componentDidUpdate(prevProps) {
     if (this.props.match.params.nid !== prevProps.match.params.nid) {
-      this.getNode(this.props.match.params.nid);
       this.getLogs(this.props.match.params.nid);
     }
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timer);
-    this.timer = null;
-  }
-
-  getNode(nid) {
-    axios.get(`/api/nodes/${nid}`).then((response) => {
-      this.setState({node: response.data.data})
-    }).catch((err) => {
-      console.error(err);
-    });
   }
 
   getLogs(nid) {
@@ -55,75 +30,11 @@ class Node extends React.Component {
     });
   }
 
-  getProgress() {
-    const {node} = this.state;
-    if (this.state.node) {
-      axios.get(`${node.address}/job/progress`).then((response) => {
-        console.log(response.data);
-        this.setState({progress: response.data.data})
-      }).catch((err) => {
-        if (err.response && err.response.status == 404) {
-          this.setState({progress: null})
-        } else if (err.response) {
-          console.error(err.response);
-        }
-      });
-    }
-  }
-
   render () {
     return (
       <Container fluid>
         <Navbar>
-          <div className="mx-auto bg-dark text-white p-3 mt-3">
-            <div className="row px-md-n2">
-              <div className="col px-md-2">
-                <FontAwesomeIcon className="mr-2" icon={faInfoCircle}></FontAwesomeIcon>
-                Status
-              </div>
-              <div className="col px-md-2">
-                {this.state.progress ? "Running" : "Polling"}
-              </div>
-            </div>
-            <div className="row px-md-n2">
-              <div className="col px-md-2">
-                <FontAwesomeIcon className="mr-2" icon={faBuilding}></FontAwesomeIcon>
-                Node
-              </div>
-              <div className="col px-md-2">
-                {this.state.node ? this.state.node.name : ""}
-              </div>
-            </div>
-            <div className="row px-md-n2">
-              <div className="col px-md-2">
-                <FontAwesomeIcon className="mr-2" icon={faLocationArrow}></FontAwesomeIcon>
-                Address
-              </div>
-              <div className="col px-md-2">
-                {this.state.node ? this.state.node.address : ""}
-              </div>
-            </div>
-            <br></br>
-            {
-              this.state.progress ? (
-                <div>
-                  <ProgressBar now={this.state.progress.percentage} label={`${this.state.progress.percentage}%`}></ProgressBar>
-                  <div className="row px-md-n2">
-                    <div className="col px-md-2">
-                      <FontAwesomeIcon className="mr-2" icon={faClock}></FontAwesomeIcon>
-                      {"Time"}
-                    </div>
-                    <div className="col px-md-2">
-                      {this.state.progress.remaining_time}
-                    </div>
-                  </div>
-                </div>
-            ) :
-              null
-            }
-          </div>
-
-
+          <NodeItem nid={this.props.match.params.nid}></NodeItem>
           <br></br>
             <Table striped bordered hover size="sm">
               <thead className="thead-dark">
--- distributor/frontend/src/components/nodes/nodeItem.js
+++ distributor/frontend/src/components/nodes/nodeItem.js
@@ -1,20 +1,108 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import axios from 'axios'
+
+import ProgressBar from 'react-bootstrap/ProgressBar'
+
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faInfoCircle, faBuilding, faLocationArrow, faClock} from '@fortawesome/free-solid-svg-icons'
 
 class NodeItem extends React.Component {
+  state = {
+    node: null,
+    logs: [],
+    progress: null,
+    status: "Offline"
+  }
+  timer = null;
+
+  componentDidMount() {
+    this.getNode(this.props.nid);
+    this.getProgress();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.nid !== prevProps.nid) {
+      this.getNode(this.nid);
+      this.getLogs(this.nid);
+    }
+  }
+
+  getNode(nid) {
+    axios.get(`/api/nodes/${nid}`).then((response) => {
+      this.setState({node: response.data.data})
+    }).catch((err) => {
+      console.error(err);
+    });
+  }
+
+  getProgress() {
+    const {node} = this.state;
+    if (this.state.node) {
+      axios.get(`${node.address}/job/progress`).then((response) => {
+        console.log(response.data);
+        this.setState({progress: response.data.data, status: "Running"})
+      }).catch((err) => {
+        if (err.response && err.response.status == 404) {
+          this.setState({progress: null, status: "Polling"})
+        } else {
+          this.setState({status: "Offline"})
+        }
+      });
+    }
+  }
+
   render () {
-    const {nid, name, address} = this.props.node;
     return (
-      <tr>
-        <th>{nid}</th>
-        <td>{name}</td>
-        <td>{address}</td>
-      </tr>
+      <div className="mx-auto bg-dark text-white p-3 mt-3">
+        <div className="row px-md-n2">
+          <div className="col px-md-2">
+            <FontAwesomeIcon className="mr-2" icon={faInfoCircle}></FontAwesomeIcon>
+            Status
+          </div>
+          <div className="col px-md-2">
+            {this.state.status}
+          </div>
+        </div>
+        <div className="row px-md-n2">
+          <div className="col px-md-2">
+            <FontAwesomeIcon className="mr-2" icon={faBuilding}></FontAwesomeIcon>
+            Node
+          </div>
+          <div className="col px-md-2">
+            {this.state.node ? this.state.node.name : ""}
+          </div>
+        </div>
+        <div className="row px-md-n2">
+          <div className="col px-md-2">
+            <FontAwesomeIcon className="mr-2" icon={faLocationArrow}></FontAwesomeIcon>
+            Address
+          </div>
+          <div className="col px-md-2">
+            {this.state.node ? this.state.node.address : ""}
+          </div>
+        </div>
+        <br></br>
+        {
+          this.state.progress ? (
+            <div>
+              <ProgressBar now={this.state.progress.percentage} label={`${this.state.progress.percentage}%`}></ProgressBar>
+              <div className="row px-md-n2">
+                <div className="col px-md-2">
+                  <FontAwesomeIcon className="mr-2" icon={faClock}></FontAwesomeIcon>
+                  {"Time"}
+                </div>
+                <div className="col px-md-2">
+                  {this.state.progress.remaining_time}
+                </div>
+              </div>
+            </div>
+        ) :
+          null
+        }
+      </div>
     );
   }
-}
-NodeItem.propTypes = {
-  node: PropTypes.object.isRequired,
 }
 
 export default NodeItem;
