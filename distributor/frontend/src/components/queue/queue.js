e8234f5513b717d4e5bbb4c264794d2570c51928
Nikolai Norum Hansen <nikolainh@hotmail.com>
2019-12-23 23:28:05 +0100

    Added jobs and with editing and deletion

--- distributor/frontend/src/components/queue/queue.js
+++ distributor/frontend/src/components/queue/queue.js
@@ -1,22 +1,88 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Navbar from '../navbar'
+import JobItem from './jobItem'
+import axios from 'axios'
+
+import Table from 'react-bootstrap/Table'
 
 class Queue extends React.Component {
   state = {
+    nodes: [],
     jobs: []
   }
 
-  getJobs() {
-
+  componentDidMount() {
+    this.getJobs();
   }
+
+  getNodes() {
+    axios.get("/api/nodes").then((response) => {
+      this.setState({nodes: response.data.data})
+    }).catch((err) => {
+      console.error(err);
+    });
+  }
+
+  getJobs() {
+    axios.get("/api/jobs")
+    .then((response) => {
+      this.setState({jobs: response.data.data});
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+  }
+
+  handleDelete = (job) => {
+    axios.delete(`/api/jobs/${job.jid}`)
+    .then((response) => {
+      console.log("Deleted");
+    })
+    .catch((err) => {
+      console.log(err.response.data);
+    });
+  };
+
+  handleEdit = (job) => {
+    const {jid, ...payload} = job;
+    axios.post(`/api/nodes/${jid}`, payload)
+    .then((response) => {
+      this.getNodes();
+    })
+    .catch((err) => {
+      console.log(err.response.data);
+    })
+  };
 
   render () {
     return (
-      <Navbar>
-        <div>
-          Queue
-        </div>
+      <Navbar nodes={this.state.nodes} updateView={() => {}}>
+        <Table striped bordered hover size="sm">
+          <thead className="thead-dark">
+            <tr>
+              <th>#</th>
+              <th>Job</th>
+              <th>Finished</th>
+              <th>Nid</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              this.state.jobs.map((job) => {
+                return (
+                  <JobItem
+                    key={job.jid}
+                    job={job}
+                    onEdit={this.handleEdit}
+                    onDelete={this.handleDelete}
+                  ></JobItem>
+                )
+              })
+            }
+          </tbody>
+        </Table>
       </Navbar>
     );
   }
