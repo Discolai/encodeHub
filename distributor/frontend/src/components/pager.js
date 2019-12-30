b5b014694688bc5bca1e586f2a3eb88b491672b6
Nikolai Norum Hansen <nikolainh@hotmail.com>
2019-12-30 11:56:58 +0100

    Added 5 times arrow and arrow disabling

--- distributor/frontend/src/components/pager.js
+++ distributor/frontend/src/components/pager.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faArrowLeft, faArrowRight} from '@fortawesome/free-solid-svg-icons'
+import {faAngleLeft, faAngleDoubleLeft, faAngleRight, faAngleDoubleRight} from '@fortawesome/free-solid-svg-icons'
 
 class Pager extends React.Component {
 
@@ -11,25 +11,31 @@ class Pager extends React.Component {
     const links = [];
 
     const start = currentPage < 5 ? 0 : currentPage-5;
-    const end = start + 10;
+    const end = totalPages < start+10 ? totalPages : start + 10;
     if (start != 0) {
-      links.push(<button className="btn btn-primary">...</button>)
+      links.push(<button key="ldot" className="btn btn-primary">...</button>)
     }
     for (let i = start; i < end; i++) {
       const classes = currentPage === i ? "btn btn-primary active" : "btn btn-primary";
       links.push(<button key={i} className={classes} onClick={() => onLink(i)}>{i}</button>);
     }
     if (end < totalPages) {
-      links.push(<button className="btn btn-primary">...</button>)
+      links.push(<button key="rdot" className="btn btn-primary">...</button>)
     }
     return (
       <div className="btn-group">
-        <button className="btn btn-primary" onClick={() => onLink(currentPage - 1)}>
-          <FontAwesomeIcon icon={faArrowLeft}></FontAwesomeIcon>
+        <button key="larrow" disabled={currentPage === 0} className="btn btn-primary" onClick={() => onLink(currentPage - 1)}>
+          <FontAwesomeIcon icon={faAngleLeft} ></FontAwesomeIcon>
+        </button>
+        <button key="ldarrow" disabled={currentPage <= 5} className="btn btn-primary" onClick={() => onLink(currentPage - 5)}>
+          <FontAwesomeIcon icon={faAngleDoubleLeft} ></FontAwesomeIcon>
         </button>
         {links}
-        <button className="btn btn-primary">
-          <FontAwesomeIcon icon={faArrowRight} onClick={() => onLink(currentPage + 1)}></FontAwesomeIcon>
+        <button key="rdarrow" disabled={currentPage >= totalPages-5} className="btn btn-primary" onClick={() => onLink(currentPage + 5)}>
+          <FontAwesomeIcon icon={faAngleDoubleRight} ></FontAwesomeIcon>
+        </button>
+        <button key="rarrow" disabled={currentPage === totalPages-1} className="btn btn-primary">
+          <FontAwesomeIcon icon={faAngleRight} onClick={() => onLink(currentPage + 1)}></FontAwesomeIcon>
         </button>
       </div>
     );
