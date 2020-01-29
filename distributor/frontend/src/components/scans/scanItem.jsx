aefe7114a3c314f09cb05078da59b978e1f0e3b7
Nikolai Norum Hansen <nikolainh@hotmail.com>
2020-01-29 19:10:56 +0100

    Implemented scan directory status

--- distributor/frontend/src/components/scans/scanItem.jsx
+++ distributor/frontend/src/components/scans/scanItem.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
-import {Card, Icon} from 'antd';
+import {Card, Button, Icon, Skeleton, notification} from 'antd';
+import io from 'socket.io-client';
 
 import InfoItem from '../infoItem';
 import ScanForm from './scanForm';
@@ -8,7 +9,36 @@ import {errorNotification} from '../../util';
 
 class ScanItem extends React.Component {
   state = {
-    scan: null
+    scan: null,
+    scanning: false,
+    lastAdded: ""
+  }
+  dataSocket = null;
+  statusSocket = null;
+
+  setSocketIoCallbacks = () => {
+    if (this.dataSocket) this.dataSocket.close();
+    this.dataSocket = io(window.location.href+"data");
+    this.dataSocket.on("scan", scan => {
+      notification.open({duration: 1, message: scan.job, placement: "bottomRight"});
+      this.setState({lastAdded: scan.job});
+    });
+    this.dataSocket.on("connect", () => console.log("connect"));
+    this.dataSocket.on("disconnect", () => console.log("disconnect"));
+
+    if (this.statusSocket) this.statusSocket.close();
+    this.statusSocket = io(window.location.href+"status");
+    this.statusSocket.on("scan", (status) => {
+      this.setState({scanning: status.scanning});
+      if (status.scanning) {
+        this.getScan();
+      }
+    });
+    this.statusSocket.on("disconnect", () => this.setState({scanning: false}));
+  }
+
+  componentDidMount() {
+    this.setSocketIoCallbacks();
   }
 
   getScan() {
@@ -23,16 +53,35 @@ class ScanItem extends React.Component {
     .catch((err) => errorNotification(err));
   };
 
+  handleStopScan = () => {
+    axios.post(`/api/scans/stop/${this.state.scan.sid}`)
+    .then((response) => notification.open({message: "Info", description: "Stopped scan"}))
+    .catch((err) => errorNotification(err));
+  }
+
   render () {
-    const {scan} = this.state;
+    const {scan, scanning} = this.state;
     return (
-      scan ? (
+      scanning ? (
         <Card style={{backgroundColor: "#001529", color: "#FFF", marginBottom: "1em"}}>
-          <InfoItem
-            infoTitle={scan.dir}
-            icon="sync"
-            iconSpin={true}
-          />
+
+          {
+            scan ? (
+              <InfoItem
+                infoTitle={scan.dir}
+                icon="sync"
+                iconSpin={true}
+                info={(
+                  <a onClick={() => this.handleStopScan()}>
+                    <Icon
+                      style={{color: "#FFF", fontSize: "2em"}}
+                      type="stop"
+                      />
+                  </a>
+                )}
+              />
+          ) : <Skeleton/>
+          }
         </Card>
       ) : (
         <ScanForm
